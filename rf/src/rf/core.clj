0b055fb95a36ed7e9660968299d0936e99187f42
Tobias Ammann <tag@adnm.ch>
2013-05-18 22:51:23 +0200

    updated rf in clojure

--- rf/src/rf/core.clj
+++ rf/src/rf/core.clj
@@ -1,14 +1,14 @@
 (ns rf.core
   (:require [clojure.zip :as zip]))
 
-(def sample [{:a 0 :b 0 :c 0 :d 0 :class 0} {:a 1 :b 0 :c 0 :d 0 :class 8}
-             {:a 0 :b 0 :c 0 :d 1 :class 1} {:a 1 :b 0 :c 0 :d 1 :class 9}
-             {:a 0 :b 0 :c 1 :d 0 :class 2} {:a 1 :b 0 :c 1 :d 0 :class :a}
-             {:a 0 :b 0 :c 1 :d 1 :class 3} {:a 1 :b 0 :c 1 :d 1 :class :b}
-             {:a 0 :b 1 :c 0 :d 0 :class 4} {:a 1 :b 1 :c 0 :d 0 :class :c}
-             {:a 0 :b 1 :c 0 :d 1 :class 5} {:a 1 :b 1 :c 0 :d 1 :class :d}
-             {:a 0 :b 1 :c 1 :d 0 :class 6} {:a 1 :b 1 :c 1 :d 0 :class :e}
-             {:a 0 :b 1 :c 1 :d 1 :class 7} {:a 1 :b 1 :c 1 :d 1 :class :f}])
+(def sample [{:a 0 :b 0 :c 0 :d 0 :class :0} {:a 1 :b 0 :c 0 :d 0 :class :8}
+             {:a 0 :b 0 :c 0 :d 1 :class :1} {:a 1 :b 0 :c 0 :d 1 :class :9}
+             {:a 0 :b 0 :c 1 :d 0 :class :2} {:a 1 :b 0 :c 1 :d 0 :class :a}
+             {:a 0 :b 0 :c 1 :d 1 :class :3} {:a 1 :b 0 :c 1 :d 1 :class :b}
+             {:a 0 :b 1 :c 0 :d 0 :class :4} {:a 1 :b 1 :c 0 :d 0 :class :c}
+             {:a 0 :b 1 :c 0 :d 1 :class :5} {:a 1 :b 1 :c 0 :d 1 :class :d}
+             {:a 0 :b 1 :c 1 :d 0 :class :6} {:a 1 :b 1 :c 1 :d 0 :class :e}
+             {:a 0 :b 1 :c 1 :d 1 :class :7} {:a 1 :b 1 :c 1 :d 1 :class :f}])
 
 (defn bootstrap [dataset]
   (let [n (count dataset)
@@ -35,19 +35,19 @@
         vs (if (<= mtry n) (take mtry (shuffle vs)) vs)]
     (reduce (fn [acc v] (reduce (fn [acc t] (conj acc [v (get t v)])) acc dataset)) #{} vs)))
 
-(defn best-split [dataset mtry]
+(defn best-split [dataset oob mtry]
   (let [[_ s l r] (first (sort (map (fn [s] (let [[l r] (select s dataset)]
                                               [(entropy l) s l r])) (splits dataset mtry))))]
-    [s (set l) (set r)]))
+    (let [[oobl oobr] (select s oob)] [s (set l) (set r) (set oobl) (set oobr)])))
 
 (defn terminal? [dataset] (= (entropy dataset) 0.0))
 
 (defn extend-node [{:keys [dataset oob] :as node} mtry]
   (if (terminal? dataset)
     (merge node {:class (or (first (first (sort-by val > (frequencies (map :class dataset))))) :unknown)})
-    (let [[s l r] (best-split dataset mtry)]
-      (merge node {:criterion s :left  {:dataset l :oob r}
-                                :right {:dataset r :oob l}}))))
+    (let [[s l r oobl oobr] (best-split dataset oob mtry)]
+      (merge node {:criterion s :left  {:dataset l :oob oobl}
+                                :right {:dataset r :oob oobr}}))))
 
 (defn rf-train [dataset ntree mtry]
   (set (pmap (fn [sample]
@@ -68,6 +68,15 @@
                         (eval-tree (:left tree)) (eval-tree (:right tree)))))]
     (first (first (sort-by val > (frequencies (filter (partial not= :unknown) (pmap eval-tree forest))))))))
 
-(def rf (rf-train sample 500 3))
+(defn tree-error [tree]
+  (let [cls (:class tree)]
+    (cond (nil? cls) (+ (tree-error (:left tree)) (tree-error (:right tree)))
+          (= cls :unknown) 0 :else (count (filter (partial = cls) (:oob tree))))))
+
+(defn rf-error [forest] (/ (reduce + 0 (pmap #(/ (tree-error %) (count (:oob %))) forest)) (count forest)))
+
+(def rf (rf-train sample 1 3))
 
 (every? #(= (rf-predict rf (dissoc % :class)) (:class %)) sample)
+
+(rf-error rf)
