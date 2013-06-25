67218f0404c8a9b1e422ba1e8e46e06db7dac37e
Tobias Ammann <tag@adnm.ch>
2013-06-25 15:45:48 +0200

    fixes to make example work better

--- rf/project.clj
+++ rf/project.clj
@@ -1,3 +1,3 @@
 (defproject rf "1.0.0-SNAPSHOT"
-  :description "An example random forest implementation."
+  :description "An examplary random forest implementation."
   :dependencies [[org.clojure/clojure "1.5.1"]])
